e76bde8224d381e53e30155985ba0cdc5f2ae97e
Antonio <230642@utxicotepec.edu.mx>
2024-11-07 14:27:35 -0600

    Practica 11
     Pantallas de Registro, Logeo y Recuperación de Contraseña

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,20 +1,17 @@
-
-const formularoLogin =(request,response) =>{
-    response.render("auth/login",{
-    auntenticado: false
+const formularioLogin = (request, response) =>   {
+    response.render("auth/login", {
+        page : "Ingresa a la plataforma"
     })
 }
 
-const formularioRegister = (request,response) =>{
-    response.render("auth/registrer",{
+const formularioRegister = (request, response) =>  {
+    response.render('auth/register', {
+        page : "Crea una nueva cuenta..."
+    })};
 
-    })
-}
-
-const formularioPasswordRecovery = (request,response) =>{
-    response.render("auth/passwordRecovery",{
-
-    })
-}
-
-export{formularoLogin,formularioRegister,formularioPasswordRecovery}
+const formularioPasswordRecovery = (request, response) =>  {
+response.render('auth/passwordRecovery', {
+        page : "Recuperación de Contraseña"
+ })};
+ 
+export {formularioLogin, formularioRegister, formularioPasswordRecovery}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [ './Views/**/*.pug'],
   theme: {
     extend: {},
   },
