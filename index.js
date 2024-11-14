5c55b115112645912233fef9095abda3230168f9
Antonio <230642@utxicotepec.edu.mx>
2024-11-13 21:34:57 -0600

    Practica 12
    Se creo las carpetas models(User.js), db(config.js), se instalaron unas dependencias necesarias, se ajusto parte de la base de datos, y se esta itentando enlazar

--- index.js
+++ index.js
@@ -2,29 +2,34 @@
 /*console.log("Hola desde NodeJS, esto esta en hot reload")*/
 
 /*const express = require('express'); */
-
-
 import express from 'express';
 import generalRoutes from './Routes/GeneralRoutes.js'
 import userRoutes from './Routes/userRouters.js'
+import db from './db/config.js'
 
 const app = express()
 
-// Configurar Templeate Engine - PUG
+// Configurar Templeate Engine - Habilitamos PUG
 app.set('view engine', 'pug')
 app.set('views','./views')
 
-//Definir la carpeta ública de recursos estáticos (assets)
+// Definir la carpeta pública de recursos estáticos (assets)
 app.use(express.static('./public'));
 
+// Conexión a la base de datos
+try {
+    await db.authenticate();
+    console.log('Conexión correcta a la base de datos')
+} catch (error) {
+    console.log(error)
+}
+
 // Configuramos nuestro servidor web
-const port =3000;
-app.listen(port, ()=>{
-    console.log(`La aplicación ha iniciado en el puerto: ${port}` );
+const port = 3000;
+app.listen(port, () => {
+    console.log(`La aplicación ha iniciado en el puerto: ${port}`);
 })
 
 // Routing - Enrutamiento para peticiones
-app.use('/',generalRoutes);
-app.use('/auth',userRoutes);
-
-app.use(express.static('./public'))
+app.use('/', generalRoutes);
+app.use('/auth', userRoutes);
